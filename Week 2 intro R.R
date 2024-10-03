d871a092c7b8169423f0baa5d9737cc2a0c1a2fa
Lucas Salgado <lvsalgado@ucdavis.edu>
2024-10-03 13:55:55 -0700

    ola

---
+++ Week 2 intro R.R
@@ -0,0 +1,34 @@
+#scientific notation
+2/10000
+
+#x=1
+x<-1
+#remove x
+rm(x)
+
+#six comparison functions
+mynumber <-6
+#question (two equal signs) = R does mynumer equals 5?
+mynumber==5
+
+#questions = R does mynumber different than 5?
+mynumber!=5
+
+#others
+mynumber <3
+
+mynumber<= -1
+
+#errors and warnings
+log("a_word")
+#nao_pode_colocar algo nao numerico dentro da funçao log
+
+#NaN é o mesmo que "not a number"
+
+#challenge
+elephant1_kg<-3492
+elephant2_lb<-7757
+elephant1_lb<-elephant1_kg *2.2
+
+myelephants<-c(elephant1_lb, elephant2_lb)
+hich(myelephants == max(myelephant2_lb))
